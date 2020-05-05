29f16664b37927a8990467982a5bcba44bd28e49
Harris Gwag <gwagdalf@gmail.com>
2020-05-05 09:31:57 +0900

    vue cli-269분

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- kebab : hello-world, Pascal : HelloWorld  -->
+    <!-- 
+      컴포넌트 명명법
+      1<hello-world></hello-world>  -- 이걸써야 vscode drill down 가능
+      2.<HelloWorld></HelloWorld>
+      3.<HelloWorld/>> 
+    -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성을 넣어 준다.
+  name: "App",
+  components: {
+    HelloWorld
+    // 'hello-world' : HelloWorld,
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+// var appHeader = {
+//   template: '<dvi>header</dvi>',
+//   methods: {
+//     addNum: function(){
+
+//     }
+//   }
+// }
+
+
+<template>
+  <!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // javascript - 인스턴스 옵션
+  methods: {
+    addNum: function() {}
+  }
+};
+</script>
+
+<style>
+/* css */
+</style>
